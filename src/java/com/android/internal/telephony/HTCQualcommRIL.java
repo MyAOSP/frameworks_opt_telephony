LineageOS/android_frameworks_opt_telephony

HTCQualcommRIL: optionally ignore IccCard states greater than 2
The htc fireball (Verizon) throws CARDSTATE_NOT_INITIALIZED corresponding
to card state 3. Include an option to ignore card states greater than 2
with an oldril prop.

Change-Id: I7c8d5cd43af1e89e47ce777d301581d6c4bc427f

src/java/com/android/internal/telephony/HTCQualcommRIL.java
@@ -41,7 +41,23 @@ public class HTCQualcommRIL extends RIL implements CommandsInterface {
     @Override
     protected Object
     responseIccCardStatus(Parcel p) {
-        Object ret = super.responseIccCardStatus(p);
+        Object ret;
+
+        boolean extraIccCardStates = needsOldRilFeature("extra_icc_card_states");
+
+        if (extraIccCardStates) {
+            int dataPosition = p.dataPosition();
+            int cardState = p.readInt();
+
+            if (cardState >= 3) {
+                ret = responseVoid(p);
+            } else {
+                p.setDataPosition(dataPosition);
+                ret = super.responseIccCardStatus(p);
+            }
+        } else {
+            ret = super.responseIccCardStatus(p);
+        }
 
         // force CDMA + LTE network mode
         boolean forceCdmaLte = needsOldRilFeature("forceCdmaLteNetworkType");

MyAOSP/frameworks_opt_telephony

src/java/com/android/internal/telephony/HTCQualcommRIL.java
@@ -41,7 +41,23 @@ public class HTCQualcommRIL extends RIL implements CommandsInterface {
     @Override
     protected Object
     responseIccCardStatus(Parcel p) {
-        Object ret = super.responseIccCardStatus(p);
+        Object ret;
+
+        boolean extraIccCardStates = needsOldRilFeature("extraicccardstates");
+
+        if (extraIccCardStates) {
+            int dataPosition = p.dataPosition();
+            int cardState = p.readInt();
+
+            if (cardState >= 3) {
+                ret = responseVoid(p);
+            } else {
+                p.setDataPosition(dataPosition);
+                ret = super.responseIccCardStatus(p);
+            }
+        } else {
+            ret = super.responseIccCardStatus(p);
+        }
 
         // force CDMA + LTE network mode
         boolean forceCdmaLte = needsOldRilFeature("forceCdmaLteNetworkType");