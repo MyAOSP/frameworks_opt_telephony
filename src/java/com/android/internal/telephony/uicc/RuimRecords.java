97e02acc78fac78533dc4ff4696ecdc7c5a772fc
baked <travp624@gmail.com>
2014-06-03 05:44:44 -0500

    fix merge
    Signed-off-by: baked <travp624@gmail.com>

--- src/java/com/android/internal/telephony/uicc/RuimRecords.java
+++ src/java/com/android/internal/telephony/uicc/RuimRecords.java
@@ -428,7 +428,7 @@ public final class RuimRecords extends IccRecords {
             if (operatorNumeric != null) {
                 if(operatorNumeric.length() <= 6) {
                     mMSIMRecordeEnabled = true;
-                    MccTable.updateMccMncConfiguration(mContext, operatorNumeric);
+                    MccTable.updateMccMncConfiguration(mContext, operatorNumeric, false);
                 }
             }
 
@@ -541,7 +541,7 @@ public final class RuimRecords extends IccRecords {
 
                 String operatorNumeric = getOperatorNumeric();
                 if (operatorNumeric != null) {
-                    if(operatorNumeric.length() <= 6){
+                    if (operatorNumeric.length() <= 6) {
                         MccTable.updateMccMncConfiguration(mContext, operatorNumeric, false);
                     }
                 }
